59f3d06dfb1573df0fc4c31507e487ad219cc775
Le Cong Danh <congdanh.le@manabie.com>
2022-11-19 09:22:39 +0700

    Run http server success

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const webPort = ":8008"
 
 func main() {
 	fmt.Println("School App")
+
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(webPort, nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("HTTP Server is running on Port: ", webPort)
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "This is my website!\n")
+}
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, HTTP!\n")
 }
