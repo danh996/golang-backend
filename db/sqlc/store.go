5357bbdce8f8063b4788cad6da81e7c291e2d25d
Le Cong Danh <congdanh.le@manabie.com>
2022-12-10 13:26:44 +0700

    implement transaction db

---
+++ db/sqlc/store.go
@@ -0,0 +1,81 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// Store provides all functions and execute db queries and transactions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+func (s *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	q := New(tx)
+
+	err = fn(q)
+
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err : %v, rb err %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+// Transfer perform a money transfer from one account to the other
+// It creates a transfer record, and account entries, and update accounts's balance within 
+// a single database transaction
+
+func (s *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := s.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+
+	return result, err
+}
