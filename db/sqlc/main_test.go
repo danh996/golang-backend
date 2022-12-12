880b51d4d2c00457ad9132f1f91aa0269adfa904
Le Cong Danh <congdanh.le@manabie.com>
2022-12-12 20:34:26 +0700

    load config from env file

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,25 +6,27 @@ import (
 	"os"
 	"testing"
 
+	"github.com/danh996/go-school/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("can not load config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("can not connect to DB", err)
 	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -7,17 +7,16 @@ import (
 
 	"github.com/danh996/go-school/api"
 	db "github.com/danh996/go-school/db/sqlc"
+	"github.com/danh996/go-school/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8089"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("can not load config", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("can not connect to DB", err)
@@ -25,7 +24,7 @@ func main() {
 
 	store := db.NewStore(conn)
 	srv := api.NewServer(store)
-	err = srv.Start(serverAddress)
+	err = srv.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("can not start server", err)
